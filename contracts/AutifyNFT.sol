5d566338a77b209941a0cdce63be86b938258c40
Advaita Saha <adv11sphs@gmail.com>
2022-05-15 18:59:53 +0530

    single-owner non-transferable custom ERC-721

--- contracts/AutifyNFT.sol
+++ contracts/AutifyNFT.sol
@@ -1,28 +1,25 @@
-//Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.6;
+ 
+import "./src/contracts/tokens/nf-token-metadata.sol";
+import "./src/contracts/ownership/ownable.sol";
+import "./Counters.sol";
+ 
+contract DeCert is NFTokenMetadata, Ownable {
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-
-contract AutifyNFT is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
-
-    constructor() ERC721("MyNFT", "NFT") {}
-
-    function mintNFT(address recipient, string memory tokenURI)
-        public onlyOwner
-        returns (uint256)
-    {
-        _tokenIds.increment();
-
-        uint256 newItemId = _tokenIds.current();
-        _mint(recipient, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-
-        return newItemId;
-    }
+    Counters.Counter private _tokenIdCounter;
+ 
+  constructor() {
+    nftName = "Autify";
+    nftSymbol = "AUTIFY";
+  }
+ 
+  function mint(address _to, string calldata _uri) external onlyOwner {
+    uint256 tokenId = _tokenIdCounter.current();
+    _tokenIdCounter.increment();
+    super._mint(_to, tokenId);
+    super._setTokenUri(tokenId, _uri);
+  }
+ 
 }
