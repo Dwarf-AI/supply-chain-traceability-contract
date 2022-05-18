117e65f7638540820a3fad5a365d1cddac5941b0
Advaita Saha <adv11sphs@gmail.com>
2022-05-19 00:04:56 +0530

    dynamic minting implemented

--- scripts/web3storage.js
+++ scripts/web3storage.js
@@ -1,10 +1,24 @@
 const { Web3Storage, File } = require('web3.storage');
 require('dotenv').config();
 const fs = require('fs');
-const mime = require('mime');
-const path = require('path');
+const { ethers } = require('ethers');
 
+const contractAddress = "0x2224e9DFcF8816148b120bEEF0944F6846e77d43";
+const artifact = require("../artifacts/contracts/AutifyNFT.sol/AutifyNFT.json")
+const providerOrUrl = {
+    kovan: process.env.KOVAN,
+    mumbai: process.env.MUMBAI,
+    rinkeby : process.env.RINKEBY
+}
 
+async function callContract(cid, type, recieversAddress) {
+    let provider = new ethers.providers.JsonRpcProvider(providerOrUrl[type]);
+    const signer = new ethers.Wallet(process.env.PVT_KEY, provider);
+    const contract_write = new ethers.Contract(contractAddress, artifact.abi, signer);
+    await contract_write.mintNFT(recieversAddress, `ipfs://${cid}/metadata.json`).catch(err => {
+        console.log(err);
+    });
+}
 
 function getAccessToken () {
     return process.env.WEB3STORAGE_TOKEN
@@ -38,7 +52,7 @@ async function main(){
         [jsonfile],
         {
             onRootCidReady : (rootCid) => {
-                console.log(rootCid);
+                callContract(rootCid, 'rinkeby', "0xF8FD64b9E74076FC2833c7de85fac6204390FA4A");
             }
         }
     );
