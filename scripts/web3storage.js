0de72c37df8b90c1f673c7b8d292b5f83a00eb63
Advaita Saha <adv11sphs@gmail.com>
2022-05-18 23:37:54 +0530

    dynamic upload script complete

--- scripts/web3storage.js
+++ scripts/web3storage.js
@@ -1,8 +1,10 @@
-const { Web3Storage, getFilesFromPath } = require('web3.storage');
+const { Web3Storage, File } = require('web3.storage');
 require('dotenv').config();
-const { fs } = require('fs');
-const { mime } = require('mime');
-const { path } = require('path');
+const fs = require('fs');
+const mime = require('mime');
+const path = require('path');
+
+
 
 function getAccessToken () {
     return process.env.WEB3STORAGE_TOKEN
@@ -11,18 +13,34 @@ function getAccessToken () {
 function makeStorageClient () {
     return new Web3Storage({ token: getAccessToken() })
 }
+
 async function fileFromPath(filePath) {
     const content = await fs.promises.readFile(filePath)
-    const type = mime.getType(filePath)
-    return new File([content], path.basename(filePath), { type })
+    return new File([content], 'image')
 }
 
 async function main(){
     const web3storage = new makeStorageClient();
     const file = await fileFromPath('./scripts/img.jpeg');
-    const rootCid = await web3storage.put([file])
-    console.log(rootCid);
+    const imageCid = await web3storage.put([file]);
+    const jsonToUpload = {
+        name: 'autify NFT',
+        description: 'autify product',
+        data : {
+            retailer : 'bata',
+            manufacturer : 'nike',
+            location : 'pune' 
+        },
+        image: `${imageCid}/image`
+    };
+    const jsonfile = new File([JSON.stringify(jsonToUpload)], 'metadata.json');
+    const jsonCid = await web3storage.put([jsonfile]);
+    console.log(imageCid,jsonCid);
 }
 
 main()
+  .catch(err => {
+      console.error(err)
+      process.exit(1)
+  })
 
